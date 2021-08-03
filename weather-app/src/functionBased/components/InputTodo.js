f1f7fd24c7aa0a8abe8ee289b527f039315c1f48
Jose Torres <josertrs@gmail.com>
2021-08-02 19:31:25 -0700

    Added svgs

--- weather-app/src/functionBased/components/InputTodo.js
+++ weather-app/src/functionBased/components/InputTodo.js
@@ -1,5 +1,5 @@
 import React, { useState }from "react";
-
+import { FaPlusCircle } from "react-icons/fa"
 const InputTodo = (props) => {
     const [inputText, setInputText] = useState({
         title: ""
@@ -34,7 +34,11 @@ const InputTodo = (props) => {
                 name="title"
                 onChange={onChange}
             />
-            <button className="input-submit">Submit</button>
+            <button className="input-submit">
+                <FaPlusCircle
+                    style={{color: "darkcyan", fontSize: "20px", marginTop: "2px"}}
+                />
+            </button>
         </form>
     );
 };
